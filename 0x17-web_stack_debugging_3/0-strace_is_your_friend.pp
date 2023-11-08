ddc87234d3102c6de9b360137b4bc9812460480b
abdelrhman mohammed <meemoo102039@gmail.com>
2023-11-08 15:20:04 +0000

    change

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,6 @@
 # Creat a manifest that fix all termintion of phpp.
 
-exec { 'ez_fix':
-  provider	=> 'shell'
-  command	=> 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path		=> ['/bin', '/usr/bin/', '/usr/loca/bin/'],
+exec {'replace':
+  provider => shell,
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }

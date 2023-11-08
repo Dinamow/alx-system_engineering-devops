14e3b04778540a712d973c0e46d2dcdf752d5d82
abdelrhman mohammed <meemoo102039@gmail.com>
2023-11-08 15:19:15 +0000

    '

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,7 @@
 # Creat a manifest that fix all termintion of phpp.
 
 exec { 'ez_fix':
-  provider	=> shell
+  provider	=> 'shell'
   command	=> 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path		=> ['/bin', '/usr/bin/', '/usr/loca/bin/'],
 }
